85ac2fb7dd314bd7c29e13a71206b9c87bebb5b5
samuel-senai-adm <samuel.a.dias@aluno.senai.br>
2026-03-04 16:01:24 -0300

    Novo2

--- DOM/atividades/atvidades.js
+++ DOM/atividades/atvidades.js
@@ -14,11 +14,33 @@ botaoVermelho.addEventListener("click", function(){
 
 // Atividada 2 - Mostrar e esconder
 
-let botaoToggle = document.querySelector("#botaoToggle")
-let mensagem = document.querySelector(".mensagem")
+let botaoToggle = document.querySelector("#botaoToggle");
+let mensagem = document.querySelector(".mensagem");
 
 botaoToggle.addEventListener("click", function () {
     mensagem.classList.toggle("oculto");
 });
 
-// Atividade 3- 
+// Atividade 3 - Contador de caracteres
+
+let texto = document.querySelector("#texto");
+let contador = document.querySelector("#contador");
+
+texto.addEventListener("keyup", function () {
+    let quantidadeDcaracter = texto.value.length;
+    contador.textContent= "Número de caracteres: " + quantidadeDcaracter;
+});
+
+// Atividade 4 - Contador de cliques
+
+let clique = document.querySelector("#clique");
+let pContador = document.querySelector("#pContador");
+let cont = 0;
+
+clique.addEventListener("click", function() {
+    cont++;
+    pContador.textContent ="Quantidade de cliques: " + cont;
+});
+
+// Atividade 5 - Criar elementos dinamicamente
+
