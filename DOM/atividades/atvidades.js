79ce153927847cd2f9a0b8627f41053400e0f92b
samuel-senai-adm <samuel.a.dias@aluno.senai.br>
2026-03-13 07:13:01 -0300

    Novo3

--- DOM/atividades/atvidades.js
+++ DOM/atividades/atvidades.js
@@ -42,5 +42,42 @@ clique.addEventListener("click", function() {
     pContador.textContent ="Quantidade de cliques: " + cont;
 });
 
-// Atividade 5 - Criar elementos dinamicamente
+// Atividade 5 - 6 - 7  Criar elementos dinamicamente
 
+let txt = document.querySelector("#txt");
+let adicionar = document.querySelector("#adiciona");
+let list = document.querySelector("#list");
+
+adicionar.addEventListener("click", function() {
+    if (txt.value === "") {
+        alert("Preencha o campo para adicionar à lista");
+        return;
+    }
+    let novoItem = document.createElement("li");
+    novoItem.textContent = txt.value;
+    list.appendChild(novoItem);
+    txt.value = ""; 
+    novoItem.addEventListener("click", function() {
+        let confirmar = confirm("Tem certeza que deseja remover este item?");
+        if (confirmar) {
+            list.removeChild(novoItem);
+        }
+    });
+});
+
+// Atividade Hard
+
+let nome = document.querySelector("#nome")
+let save = document.querySelector("#save")
+let paragrafo = document.querySelector("#p");
+
+save.addEventListener("click", function() {
+    if (nome.value.trim() === "") {
+        p.textContent = "O campo nome é obrigatório";
+        p.style.color ="red";
+    } else{
+     (nome.value.trim() === paragrafo.textContent)
+        p.textContent = "Nome adicionado com sucesso!";
+        p.style.color ="green";
+    }}
+);
